8b12e3cb382a727dd4b97a3e01ee9ffb9b172e9d
noman-sde <noman.suhel.ahmed@gmail.com>
2026-06-21 01:38:42 +0530

    June 21 midnight

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thurstStrength = 1f;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float rotationStrength = 1f;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+  void OnEnable()
+  {
+    thrust.Enable();
+    rotation.Enable();
+  }
+  void FixedUpdate()
+  {
+    ProcessThrust();
+    ProcessRotation();
+  }
+
+  private void ProcessThrust()
+  {
+    if (thrust.IsPressed())
     {
-        
+      rb.AddRelativeForce(Vector3.up * thurstStrength * Time.fixedDeltaTime);
+    }
+  }
+  private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput != 0)
+        {
+            rb.AddRelativeTorque(Vector3.back * rotationInput * rotationStrength * Time.fixedDeltaTime);
+        }
     }
 }
