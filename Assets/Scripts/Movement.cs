7b528a9008ec23a216e7c929aba533b6b87b2ae3
noman-sde <noman.suhel.ahmed@gmail.com>
2026-06-25 20:50:31 +0530

    25 June

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -53,6 +53,8 @@ public class Movement : MonoBehaviour
   }
   private void ApplyRotation(float rotationThisFrame)
   {
+    rb.freezeRotation = true; // freezing rotation so we can manually rotate
     transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
   }
 }
